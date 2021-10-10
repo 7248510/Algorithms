374d6613bf7f0f970465f6eb426978c4e0ac8182
Caleb <56181475+7248510@users.noreply.github.com>
2021-10-10 10:39:16 -0400

    Update map.cc

--- map.cc
+++ map.cc
@@ -1,7 +1,25 @@
+//cplusplus.com
+//Stackoverflow
 #include <unordered_map>
 #include <iostream>
-
+#include <string>
 int main()
 {
+  std::unordered_map<std::string, int> mapInst;
+  
+  mapInst["Hello"] = 0;
+  mapInst["World"] = 1;
+  mapInst["Map"] = 2;
+  mapInst["findMe"] = 3;
+    auto search = mapInst.find("findMe");
+
+    if (search == mapInst.end()) {
+        std::cout << "Not found: " << std::endl;
+    }
+
+    else {
+        std::cout << "Found: " << std::endl;
+        std::cout << search->first << " " << search->second << std::endl;
+    }
   return 0;
 }
